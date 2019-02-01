acb0e440444c1d72f6a5a7b3c42a63ca54e7db0a
theHarshit1 <harshit@github>
2019-02-01 21:10:50 +0530

    call logs to excel file

--- app/src/main/java/com/example/incomingreceiver/MainActivity.java
+++ app/src/main/java/com/example/incomingreceiver/MainActivity.java
@@ -4,27 +4,69 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.TelecomManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
+    static Workbook wb;
+    public static Sheet s;
+    public static int call_count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button audio=(Button) findViewById(R.id.audio);
+        Button logout=(Button) findViewById(R.id.logout);
         audio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,AudioPlaying.class));
+            }
+        });
+        wb = new HSSFWorkbook();
+        //New Sheet
+        s = MainActivity.wb.createSheet("CAllLog");
+        call_count=0;
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File file = new File(Environment.getExternalStorageDirectory(), "Call Logs.xls");
+                FileOutputStream os = null;
+
+                try {
+                    os = new FileOutputStream(file);
+                    wb.write(os);
+                    Log.w("FileUtils", "Writing file" + file);
+                } catch (IOException e) {
+                    Log.w("FileUtils", "Error writing " + file, e);
+                } catch (Exception e) {
+                    Log.w("FileUtils", "Failed to save file", e);
+                } finally {
+                    try {
+                        if (null != os)
+                            os.close();
+                    } catch (Exception ex) {
+                    }
+                    startActivity(new Intent(MainActivity.this,EmployeeLogin.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                }
             }
         });
 
--- app/src/main/java/com/example/incomingreceiver/ServiceReceiver.java
+++ app/src/main/java/com/example/incomingreceiver/ServiceReceiver.java
@@ -5,6 +5,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.inputmethodservice.Keyboard;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v4.content.ContextCompat;
 import android.telecom.TelecomManager;
 import android.telephony.TelephonyManager;
@@ -14,13 +17,35 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import static com.example.incomingreceiver.MainActivity.call_count;
+
 
 public class ServiceReceiver extends BroadcastReceiver{
+
+    final MediaRecorder uprecorder = new MediaRecorder();
+    final MediaRecorder downrecorder=new MediaRecorder();
+    static boolean init;
+    long start_time,end_time;
+    static final String path1=Environment.getExternalStorageDirectory().getAbsolutePath()+"/call records/recording.3GP",path2=Environment.getExternalStorageDirectory().getAbsolutePath()+"/call records/downlink.3GP";
 
     @Override
     public void onReceive(Context context, Intent intent){
         String uid=FirebaseAuth.getInstance().getUid();
         DatabaseReference ref=FirebaseDatabase.getInstance().getReference("audio/"+uid);
+        init=false;
         try{
             String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
 
@@ -39,15 +64,63 @@ public class ServiceReceiver extends BroadcastReceiver{
                 }
             }
 
-            if(state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
+            if(state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){         //call is picked up
+                start_time = System.currentTimeMillis();
                 Toast.makeText(context, "Call Received", Toast.LENGTH_LONG).show();
+                // make sure the directory we plan to store the recording in exists
+
+                File directory1 = new File(path1).getParentFile();
+                File directory2 = new File(path2).getParentFile();
+                if (!directory1.exists() && !directory1.mkdirs() || !directory2.exists() && !directory2.mkdirs()) {
+                    throw new IOException("Path to file could not be created.");
+                }
+
                 ref.setValue("start");
+
+//                uprecorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
+//                uprecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+//                uprecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+//                uprecorder.setOutputFile(path1);
+//                uprecorder.prepare();
+//                uprecorder.start();
+
+//                downrecorder.setAudioSource(MediaRecorder.AudioSource.VOICE_DOWNLINK);
+//                downrecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+//                downrecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+//                downrecorder.setOutputFile(path2);
+//                downrecorder.prepare();
+//                downrecorder.start();
+                init=true;
+
+                String number=TelephonyManager.EXTRA_INCOMING_NUMBER;
+                long totalTime = end_time - start_time;
+                Calendar cal = Calendar.getInstance();
+                Date date = cal.getTime();
+                DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+                String formattedDate = dateFormat.format(date);
+
+                Cell c = null;
+                Row row = MainActivity.s.createRow(call_count);
+
+                c = row.createCell(0);
+                c.setCellValue(number);
+                c = row.createCell(1);
+                c.setCellValue(totalTime);
+                c = row.createCell(2);
+                c.setCellValue(formattedDate);
+                call_count++;
 
             }
 
             if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)){
+                end_time = System.currentTimeMillis();
                 Toast.makeText(context, "Phone Is Idle", Toast.LENGTH_LONG).show();
                 ref.setValue("stop");
+//                    uprecorder.stop();
+//                    uprecorder.release();
+//                    downrecorder.stop();
+//                    downrecorder.release();
+                    init=false;
             }
         }
         catch(Exception e){e.printStackTrace();}
